00df2d72f5e139c867e8c2f2c5f7c38890213f75
Petty <pettwil1131@gmail.com>
2021-07-12 17:18:42 -0500

    Adds Home.vue styling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,6 @@
 <template>
   <div class="home">
-    <!-- Begin Header -->
-    <div class="py-5 bg-secondary">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12 mx-auto">
-            <h1 class="text-white font-weight-light">
-              <span class="font-weight-bold">ResX</span> Login
-            </h1>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- End Header -->
-    <h1>{{ message }}</h1>
-    <form v-on:submit.prevent="submit()">
+    <!-- <form v-on:submit.prevent="submit()">
       <ul>
         <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
       </ul>
@@ -28,7 +14,93 @@
       </div>
       <input type="submit" value="Login" />
       <router-link to="/signup" tag="button">Signup</router-link>
-    </form>
+    </form> -->
+    <!-- Begin Header -->
+    <div class="py-5 bg-secondary">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 mx-auto">
+            <h1 class="text-white font-weight-light">
+              <span class="font-weight-bold">ResX</span> Login
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- End Header -->
+    <!-- begin login -->
+    <div class="bg-light">
+      <div class="container">
+        <div class="row justify-content-center align-items-center d-flex">
+          <div class="col-md-4 mx-auto">
+            <div class="osahan-login py-4">
+              <div class="text-center mb-4">
+                <a href="/"><img src="/img/logo.svg" alt="" /></a>
+                <h5 class="font-weight-bold mt-3">Welcome Back</h5>
+                <p class="text-muted">Get Outside</p>
+              </div>
+              <form v-on:submit.prevent="submit()">
+                <ul>
+                  <li v-for="error in errors" v-bind:key="error">
+                    {{ error }}
+                  </li>
+                </ul>
+
+                <!-- begin email/password -->
+                <div class="form-group">
+                  <label class="mb-1">Email</label>
+                  <div class="position-relative icon-form-control">
+                    <i class="feather-user position-absolute"></i>
+                    <input type="email" v-model="email" class="form-control" />
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label class="mb-1">Password</label>
+                  <div class="position-relative icon-form-control">
+                    <i class="feather-unlock position-absolute"></i>
+                    <input
+                      type="password"
+                      v-model="password"
+                      class="form-control"
+                    />
+                  </div>
+                </div>
+                <!-- end email/password -->
+
+                <!-- password remember -->
+                <!-- <div class="custom-control custom-checkbox mb-3">
+                  <input
+                    type="checkbox"
+                    class="custom-control-input"
+                    id="customCheck1"
+                  />
+                  <label class="custom-control-label" for="customCheck1"
+                    >Remember password</label
+                  >
+                </div> -->
+                <!-- end password remember -->
+
+                <button
+                  class="btn btn-primary btn-block text-uppercase"
+                  type="submit"
+                >
+                  Sign in
+                </button>
+                <div class="d-flex justify-content-center">
+                  <span class="m-auto pt-3">
+                    New to ResX?
+                    <router-link class="font-weight-bold" to="/signup"
+                      >Join now</router-link
+                    ></span
+                  >
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- end login -->
   </div>
 </template>
 
@@ -40,7 +112,6 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "ResX Login",
       email: "",
       password: "",
       errors: [],
--- src/views/listings/New.vue
+++ src/views/listings/New.vue
@@ -1,19 +1,6 @@
 <template>
   <div class="listings-new">
-    <!-- Begin Header -->
-    <div class="py-5 bg-secondary">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12 mx-auto">
-            <h1 class="text-white font-weight-light">
-              <span class="font-weight-bold">ResX</span> New Listing
-            </h1>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- End Header -->
-    <form v-on:submit.prevent="newListing()">
+    <!-- <form v-on:submit.prevent="newListing()">
       <ul>
         <li class="text-danger" v-for="error in errors" v-bind:key="error">
           {{ error }}
@@ -61,8 +48,128 @@
         />
       </div>
       <input type="submit" class="btn btn-primary" value="Submit" />
-    </form>
-    {{ newListingParams }}
+    </form> -->
+    <!-- Begin Header -->
+    <div class="py-5 bg-secondary">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 mx-auto">
+            <h1 class="text-white font-weight-light">
+              <span class="font-weight-bold">ResX</span> New Listing
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- End Header -->
+
+    <!-- Begin Form -->
+    <div class="bg-light">
+      <div class="container bg-light">
+        <!-- <div
+          class="row justify-content-center align-items-center d-flex vh-100"
+        > -->
+        <div class="col-md-4 mx-auto">
+          <div class="osahan-login py-4">
+            <div class="text-center mb-4">
+              <a href="/"><img src="/img/logo.svg" alt="" /></a>
+              <h5 class="font-weight-bold mt-3">New Listing</h5>
+            </div>
+            <form v-on:submit.prevent="newListing()">
+              <ul>
+                <li
+                  class="text-danger"
+                  v-for="error in errors"
+                  v-bind:key="error"
+                >
+                  {{ error }}
+                </li>
+              </ul>
+              <div class="form-group">
+                <label class="mb-1">Title</label>
+                <div class="position-relative">
+                  <input
+                    type="text"
+                    v-model="editListingParams.title"
+                    class="form-control"
+                  />
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="mb-1">Address</label>
+                <div class="position-relative">
+                  <input
+                    v-model="editListingParams.address"
+                    class="form-control"
+                  />
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="mb-1">Description</label>
+                <div class="position-relative">
+                  <textarea
+                    rows="7"
+                    cols="10"
+                    v-model="editListingParams.description"
+                    class="form-control"
+                  ></textarea>
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="mb-1">Price</label>
+                <div class="position-relative">
+                  <input
+                    type="text"
+                    v-model="editListingParams.price"
+                    class="form-control"
+                  />
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="mb-1">Availability</label>
+                <div class="position-relative">
+                  <textarea
+                    rows="5"
+                    cols="10"
+                    v-model="editListingParams.availability"
+                    class="form-control"
+                  ></textarea>
+                </div>
+              </div>
+              <div>
+                <button
+                  class="btn btn-primary btn-block text-uppercase mb-3"
+                  type="submit"
+                >
+                  Submit
+                </button>
+              </div>
+              <div class="form-group">
+                <label class="mb-1"
+                  >You agree to the ResX <a href="#">User Agreement</a>,
+                  <a href="#">Privacy Policy</a>, and
+                  <a href="#">Cookie Policy</a>.</label
+                >
+              </div>
+            </form>
+
+            <div class="form-group">
+              <div class="tab-content" id="myTabContent">
+                <div
+                  class="tab-pane fade show active"
+                  id="home"
+                  role="tabpanel"
+                  aria-labelledby="home-tab"
+                ></div>
+              </div>
+            </div>
+            <div></div>
+          </div>
+        </div>
+        <!-- </div> -->
+      </div>
+    </div>
+    <!-- End Form -->
   </div>
 </template>
 
