d581fb2470ebed696ac83c122851cb46654be344
Petty <pettwil1131@gmail.com>
2021-07-29 15:42:30 -0500

    Changes baseurl

--- src/main.js
+++ src/main.js
@@ -7,7 +7,9 @@ import Vue2Filters from "vue2-filters";
 Vue.use(Vue2Filters);
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+  process.env.NODE_ENV === "development"
+    ? "https://resxleasing.herokuapp.com/"
+    : "/";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
