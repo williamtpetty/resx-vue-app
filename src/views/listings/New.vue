c6c0e84416d085f39c422ba636bd838942b19a04
Petty <pettwil1131@gmail.com>
2021-07-30 16:11:04 -0500

    Adjusts axios post request

--- src/views/listings/New.vue
+++ src/views/listings/New.vue
@@ -1,54 +1,5 @@
 <template>
   <div class="listings-new">
-    <!-- <form v-on:submit.prevent="newListing()">
-      <ul>
-        <li class="text-danger" v-for="error in errors" v-bind:key="error">
-          {{ error }}
-        </li>
-      </ul>
-
-      <div class="form-group">
-        <label>Title: </label>
-        <input
-          type="text"
-          class="form-control"
-          v-model="newListingParams.title"
-        />
-      </div>
-      <div class="form-group">
-        <label>Description </label>
-        <textarea
-          type="text"
-          class="form-control"
-          v-model="newListingParams.description"
-        ></textarea>
-      </div>
-      <div class="form-group">
-        <label>Address: </label>
-        <input
-          type="text"
-          class="form-control"
-          v-model="newListingParams.address"
-        />
-      </div>
-      <div class="form-group">
-        <label>Availability: </label>
-        <textarea
-          type="text"
-          class="form-control"
-          v-model="newListingParams.availability"
-        ></textarea>
-      </div>
-      <div class="form-group">
-        <label>Price: </label>
-        <input
-          type="text"
-          class="form-control"
-          v-model="newListingParams.price"
-        />
-      </div>
-      <input type="submit" class="btn btn-primary" value="Submit" />
-    </form> -->
     <!-- Begin Header -->
     <div class="py-5" style="background-color: #b0c4ed">
       <div class="container">
@@ -64,7 +15,7 @@
     <!-- End Header -->
 
     <!-- Begin Form -->
-    <div class="bg-white vh-100">
+    <div class="bg-white">
       <div class="container">
         <!-- <div
           class="row justify-content-center align-items-center d-flex vh-100"
@@ -189,10 +140,10 @@ export default {
   methods: {
     newListing: function () {
       axios
-        .post(`listings`, this.newListingParams)
+        .post(`/listings`, this.newListingParams)
         .then((response) => {
           console.log(response.data);
-          this.$router.push(`listings/${response.data.id}/edit`);
+          this.$router.push(`/listings/${response.data.id}/edit`);
         })
         .catch((error) => {
           this.errors = error.response.data.errors;
