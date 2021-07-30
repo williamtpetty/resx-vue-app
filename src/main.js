7e2833878d01d65ebe6ea76f792fb92c48ea5223
Petty <pettwil1131@gmail.com>
2021-07-30 16:28:08 -0500

    Adds baseURL the correct way

--- src/main.js
+++ src/main.js
@@ -7,9 +7,9 @@ import Vue2Filters from "vue2-filters";
 Vue.use(Vue2Filters);
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
-
-// axios.defaults.baseURL = "https://resxleasing.herokuapp.com";
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://resxleasing.herokuapp.com";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
